fc258c08a8943c00eb61d0342be55baccfdc0568
Aribzhan <aribzankamilzanov@gmail.com>
2026-01-23 13:26:37 +0500

    new tasks

--- lesson-3/js/script.js
+++ lesson-3/js/script.js
@@ -1,8 +1,27 @@
-let button = document.getElementById('btn1');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let numInput = document.getElementById('numInput');
+let title = document.getElementById('title');
+let btn3 = document.getElementById('btn3');
+
 
 function btn1OnClick() {
     alert('You clicked the button!');
 }
 
+function btn2OnClick(number) {
+    console.log("Квадрат числа:", number * number);
+}
 
-button.addEventListener('click', btn1OnClick);
+function btn3OnClick() {
+    title.textContent = "Hello  Aribzhan";
+}
+
+
+btn1.addEventListener('click', btn1OnClick);
+btn2.addEventListener('click', function(){
+    let number = Number(numInput.value);
+    btn2OnClick(number);
+});
+
+btn3.addEventListener('click', btn3OnClick);
